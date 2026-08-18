bc2bdcb9642c90176b83f77d92ea211f5ef87b64
nababich <nababich@dengisrazy.ru>
2026-08-18 10:29:55 +0300

    add start page

--- src/pages/index/(index).vue
+++ src/pages/index/(index).vue
@@ -1,12 +1,23 @@
 <template>
   <q-page class="flex flex-center">
-    <div>
-      Выберите диалог слева
+    <div class="start-page">
+      <ChatInput @send="onSend" />
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-// пока пусто
+import ChatInput from "@/components/ChatInput.vue";
+
+const onSend = (message: string) => {
+  console.log("Отправлено:", message);
+};
 </script>
 
+<style scoped lang="scss">
+.start-page {
+  width: 100%;
+  max-width: 900px;
+  padding: 16px;
+}
+</style>
